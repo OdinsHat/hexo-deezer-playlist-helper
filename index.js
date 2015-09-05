be6257ed8737b4eb1cfc4c81cca69c6d55fa25c5
Doug Bromley <doug.bromley@gmail.com>
2015-09-05 15:57:08 +0100

    Slight change to tag name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
-    
-    hexo.extend.helper.register('deezer-playlist', function(playlistId, width, height){
+
+    hexo.extend.helper.register('deezerpl', function(playlistId, width, height){
         return '<iframe scrolling="no" frameborder="0" allowTransparency="true" src="http://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=' + width + '&height=' + height + '&color=1990DB&layout=dark&size=medium&type=playlist&id=' + playlistId + '&title=" width="' + width + '" height="' + height + '"></iframe>';
     });
 })();
