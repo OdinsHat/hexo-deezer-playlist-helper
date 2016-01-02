d32845b0181be71623fe633ed8e914cba58e3b98
Doug Bromley <doug.bromley@gmail.com>
2016-01-02 02:41:22 +0000

    Simplyfying code to make testing easier

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-(function () {
-    'use strict';
-
-    hexo.extend.helper.register('deezerpl', function(playlistId, width, height){
-        return '<iframe scrolling="no" frameborder="0" allowTransparency="true" src="http://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=' + width + '&height=' + height + '&color=1990DB&layout=dark&size=medium&type=playlist&id=' + playlistId + '&title=" width="' + width + '" height="' + height + '"></iframe>';
-    });
-})();
+hexo.extend.helper.register('deezerpl', function(id, width, height){
+    return '<iframe scrolling="no" frameborder="0" allowTransparency="true" src="http://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=' + width + '&height=' + height + '&color=1990DB&layout=dark&size=medium&type=playlist&id=' + id + '&title=" width="' + width + '" height="' + height + '"></iframe>';
+});
